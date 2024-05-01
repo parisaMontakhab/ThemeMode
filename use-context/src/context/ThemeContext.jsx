59457b84b6ebda244cf49b5cf2db7745364f7d94
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-01 13:30:45 +0200

    refactor: return provide in ThemeProvider

--- use-context/src/context/ThemeContext.jsx
+++ use-context/src/context/ThemeContext.jsx
@@ -1,3 +1,11 @@
 import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
+
+export function ThemeProvider(){
+    return(
+        <ThemeContext.Provider>
+
+        </ThemeContext.Provider>
+    )
+}
