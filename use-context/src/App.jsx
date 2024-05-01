7c058cb60e5dac1e4bb799dcd47ae93e9baa6c33
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-01 12:29:36 +0200

    feat: use provider in App compnnt

--- use-context/src/App.jsx
+++ use-context/src/App.jsx
@@ -1,27 +1,28 @@
 import { createContext, useState } from "react";
-import Form from "./assets/components/Form"
-import './App.css'
+import Form from "./assets/components/Form";
+import "./App.css";
 
-const ThemeContext = createContext()
+const ThemeContext = createContext();
 
 function App() {
-  
   const [theme, setTheme] = useState("light");
   return (
-    <div>
-      <Form  />
-      <label>
-        <input
-          type="checkbox"
-          checked={theme === "dark"}
-          onChange={(e) => {
-            setTheme(e.target.checked ? "dark" : "light");
-          }}
-        />
-        Use dark mode
-      </label>
-    </div>
-  )
+    <ThemeContext>
+      <div>
+        <Form />
+        <label>
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={(e) => {
+              setTheme(e.target.checked ? "dark" : "light");
+            }}
+          />
+          Use dark mode
+        </label>
+      </div>
+    </ThemeContext>
+  );
 }
 
-export default App
+export default App;
